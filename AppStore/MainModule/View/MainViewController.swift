ec113fac0d763e33d2b573fdc158c2c0278a55ee
Alexandr <74298908+Alexandr205@users.noreply.github.com>
2022-10-20 13:28:42 +0600

    MainVC bag fixed

--- AppStore/MainModule/View/MainViewController.swift
+++ AppStore/MainModule/View/MainViewController.swift
@@ -38,6 +38,7 @@ class MainViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        viewModel.didGetOrderList()
         orderTableView.reloadData()
     }
     
